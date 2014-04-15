48b0ff7708a9e8ed21372bf70cd8b530759b78af
chronos <cenix.stylo@gmail.com>
2014-04-15 13:14:47 +0300

    indexer initial import

---
+++ indexer/src/Index.scala
@@ -0,0 +1,60 @@
+import idx.{Feed2Solr, SolrId}
+import scala.collection.mutable.ArrayBuffer
+import scala.xml._
+object Main
+{
+  def main(args: Array[String])
+  {
+    var n = 1;
+    val lst: ArrayBuffer[SolrId] = ArrayBuffer[SolrId]();
+
+    // Read a local Posts.xml file to be indexed.
+    // We currently support Posts.xml only, since it provides all requirements.
+    var Posts = scala.io.Source.fromFile("D:\\stackoverflow\\Posts.xml");
+
+    // Iterate over all lines, and extract meaningful rows of data, and build a SOLR
+    // object from them.
+    for (post <- Posts.getLines())
+    {
+      if (post.startsWith("  <row"))
+      {
+
+        // The purpose of this parameter is to handle failure.
+        // If we fail while indexing, we can tell it to start from a later index by changing the condition.
+        if (n > 0)
+        {
+          val row = XML.loadString(post);
+          val SolrDoc = new SolrId((row \ "@Id").toString(),
+            (row \ "@PostTypeId").toString(),
+            (row \ "@ParentId").toString(),
+            (row \ "@AcceptedAnswerId").toString(),
+            (row \ "@CreationDate").toString(),
+            (row \ "@Score").toString(),
+            (row \ "@Body").toString(),
+            (row \ "@OwnerUserId").toString(),
+            (row \ "@LastActivityDate").toString(),
+            (row \ "@Title").toString(),
+            (row \ "@Tags").toString(),
+            (row \ "@AnswerCount").toString())
+
+          //println(SolrDoc.toString)lst
+          lst += SolrDoc;
+
+        }
+
+        n = n + 1
+      }
+
+      // Send results to SOLR as a bulk to prevent network bottleneck.
+      if ((lst.size + 1) % 40001 == 0)
+      {
+        println (lst.size)
+        (new Feed2Solr).send(lst)
+        lst.clear();
+
+      }
+    }
+
+    println(n)
+  }
+}
---
+++ indexer/src/idx/SolrId.scala
@@ -0,0 +1,46 @@
+package idx
+
+import org.apache.solr.common.SolrInputDocument
+import java.util.ArrayList
+import org.apache.solr.client.solrj.impl.HttpSolrServer
+import org.apache.solr.morphlines.solr.SolrMorphlineContext
+import scala.collection.mutable.ArrayBuffer
+
+case class SolrId(Id:String,PostTypeId:String,ParentId:String, AcceptedAnswerId:String,CreationDate:String,Score:String, Body:String,
+                  OwnerUserId:String, LastActivityDate:String, Title:String, Tags:String, AnswerCount:String)
+class Feed2Solr
+{
+  // The SOLR URL we would like to index to.
+  val url = "http://localhost:8983/solr/"
+  val solrDocuments = new ArrayList[SolrInputDocument]()
+  val server= new HttpSolrServer( url )
+
+  // Send a bulk of SOLR documents to SOLR.
+  def send(posts:ArrayBuffer[SolrId])
+  {
+      val docs: ArrayList[SolrInputDocument] = new ArrayList[SolrInputDocument]();
+      posts.foreach(post=>docs.add(getSolrDocument(post)))
+      server.add(docs);
+      server.commit();
+  }
+
+  // Build a SolrInputDocument and return it.
+  def getSolrDocument(post: SolrId): SolrInputDocument =
+  {
+    val document = new SolrInputDocument()
+    document.addField("Id",post.Id);
+    document.addField("PostTypeId",post.PostTypeId);
+    document.addField("ParentId",post.ParentId);
+    document.addField("AcceptedAnswerId",post.AcceptedAnswerId);
+    document.addField("CreationDate",post.CreationDate + "Z");
+    document.addField("Score",post.Score);
+    document.addField("Body",post.Body);
+    document.addField("OwnerUserId",post.OwnerUserId);
+    document.addField("LastActivityDate",post.LastActivityDate + "Z");
+    document.addField("Title",post.Title);
+    document.addField("Tags",post.Tags);
+    document.addField("AnswerCount",post.AnswerCount);
+
+    document
+  }
+}
